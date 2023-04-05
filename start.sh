13653e4e5e1f8e926123d02e5dead7d838fb63db
Alucardx2 <88268261+Alucardx2@users.noreply.github.com>
2023-04-05 19:54:36 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/kdbotz/UrlShortner-AutoFilter-Bot.git /UrlShortner-AutoFilter-Bot
+  git clone https://github.com/Alucardx2/revive_autoheaven
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /UrlShortner-AutoFilter-Bot
+  git clone https://github.com/Alucardx2/revive_autoheaven
 fi
-cd /UrlShortner-AutoFilter-Bot
+cd /revive_autoheaven
 pip3 install -U -r requirements.txt
 echo "Starting TargetX...."
 gunicorn app:app & python3 bot.py
