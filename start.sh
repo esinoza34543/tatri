2fe13b70d4ea03e8cb542efa1eb6cb5e165d65bb
Alucardx2 <88268261+Alucardx2@users.noreply.github.com>
2023-04-05 20:10:03 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Alucardx2/revive_autoheaven
+  git clone https://github.com/Alucardx2/revive_autoheaven.git /revive_autoheaven
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone https://github.com/Alucardx2/revive_autoheaven
+  git clone https://github.com/Alucardx2/revive_autoheaven.git /revive_autoheaven
 fi
 cd /revive_autoheaven
 pip3 install -U -r requirements.txt
