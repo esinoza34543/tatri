0cb3bcc3d533ac4c5922532d4f20ed9a85555207
Alucardx2 <88268261+Alucardx2@users.noreply.github.com>
2023-04-06 11:23:10 +0530

    Update info.py

--- info.py
+++ info.py
@@ -50,7 +50,7 @@ SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'Heavens Chat')
 MSG_ALRT = environ.get('MSG_ALRT', 'Share and Support Us')
 
 # Custom Chats
-SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', ''))
+SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', '0'))
 HOW_DWLD_LINK = environ.get('HOW_DWLD_LINK', 'https://t.me/HeavenForYouAll/7878')
 
 # Log Channels
